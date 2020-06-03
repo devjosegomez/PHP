282095588516937153e8f0427f80bcffe9894f79
devjosegomez <devjosegomez@gmail.com>
2020-06-03 06:17:13 -0500

    usage of  var_dump

--- 01_HelloWorld/HelloWorld.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "Hello world, 2020 please do not kill me..."
-?>
---
+++ php_basics/HelloWorld.php
@@ -0,0 +1,9 @@
+<?php
+    $message = "it's just a joke!";
+    $count = 3;
+    $price = 3.50;
+
+    echo "Hello world, 2020 please do not kill me... ";
+    echo $message;
+    var_dump($count , $price);
+?>
