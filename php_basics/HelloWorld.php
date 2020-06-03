81984fba3c7cdbc33bfcb6714f8d308e6426d802
devjosegomez <devjosegomez@gmail.com>
2020-06-03 06:35:47 -0500

    Type juggling & variable interpolation

--- php_basics/HelloWorld.php
+++ php_basics/HelloWorld.php
@@ -2,8 +2,20 @@
     $message = "it's just a joke!";
     $count = 3;
     $price = 3.50;
+    $is_admin = true;
+    $data = null;
 
     echo "Hello world, 2020 please do not kill me... ";
-    echo $message;
-    var_dump($count , $price);
+    echo $message . "<br>";
+    var_dump($count , $price, $is_admin, $data);
+
+    //Example of type juggling
+    $cows = "210";
+    $corn = 3;
+    var_dump($cows * $corn);
+
+    //Example of Variable interpolation (You do not need a dot to concatenate)
+    $user = "José";
+    $warning = "you should not be here!";
+    echo "$user, $warning";
 ?>
