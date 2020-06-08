c037fda1eec8a8cf4cacd23ad2e2a148be5ba16a
devjosegomez <devjosegomez@gmail.com>
2020-06-08 06:38:38 -0500

    First query in MySQL with PHP

--- php_db/index.php
+++ php_db/index.php
@@ -7,11 +7,26 @@
 
     $conn = mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
 
-    if ( mysqli_connect_error()) {
-        echo ( mysqli_connect_error());
+    if ( mysqli_connect_error() ) {
+        echo mysqli_connect_error();
         exit;
     }
 
     echo "Connected successfully";
+
+    //Example of query
+    $query = "SELECT *
+              FROM article
+              ORDER by published_at;";
+    
+    $results = mysqli_query( $conn, $query );
+    if ( $results === false ) {
+        echo mysqli_connect_error();
+        exit;
+    } else {
+        //Associative array
+        $articles = mysqli_fetch_all( $results, MYSQLI_ASSOC );
+        var_dump ( $articles );
+    }
 
 ?>
